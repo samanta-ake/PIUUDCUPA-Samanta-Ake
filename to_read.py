f56ef4cfcf8e5e46ef69cb94112f101318ad5736
Samanta Āķe <samanta.ake@va.lv>
2026-09-15 10:50:24 +0300

    Pievienota gramatas atzimesana par izlasitu

--- to_read.py
+++ to_read.py
@@ -9,7 +9,8 @@ books = [
 print()
 print("1. Parādīt grāmatas")
 print("2. Pievienot grāmatu")
-print("3. Iziet")
+print("3. Atzīmēt kā izlasītu")
+print("4. Iziet")
 
 choice = input("Izvēlies darbību: ")
 
@@ -21,3 +22,7 @@ elif choice == "2":
     books.append([new_book, "nav izlasīta"])
     print("Grāmata pievienota!")
     print("Pievienotā grāmata:", new_book)
+elif choice == "3":
+    book_number = int(input("Kuras grāmatas numuru atzīmēt kā izlasītu? "))
+    books[book_number - 1][1] = "izlasīta"
+    print("Grāmata atzīmēta kā izlasīta!")
