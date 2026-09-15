1958a5c255abc9c4798443ef2fb23a76da6cf624
Samanta Āķe <samanta.ake@va.lv>
2026-09-15 10:44:41 +0300

    Pievienota darbibas izvele

--- to_read.py
+++ to_read.py
@@ -14,3 +14,11 @@ for book in books:
     print()
 print("1. Parādīt grāmatas")
 print("2. Iziet")
+
+choice = input("Izvēlies darbību: ")
+
+if choice == "1":
+    for book in books:
+        print("-", book[0], "-", book[1])
+elif choice == "2":
+    print("Programma beidzas.")
